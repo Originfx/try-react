291863fbf68729ba61698362fc081f1d957c2a24
OriginFX <originfx@yandex.ru>
2022-07-11 08:07:43 +0300

    Счёт

--- frontend/src/components/typer/Typer.jsx
+++ frontend/src/components/typer/Typer.jsx
@@ -6,72 +6,74 @@ import "./style.css";
 
 function Typer () {
     let [area, setArea] = useState("Я видел такое, во что вы, люди, просто не поверите. Штурмовые корабли в огне на подступах к Ориону. Я смотрел, как Си-лучи мерцают во тьме близ врат Тангейзера. Все эти мгновения исчезнут во времени, как слёзы под дождём. Пора умирать.");
-    let [targetWord, setTargetWord] = useState("");
     let [inputWord, setInputWord] = useState("");
     let [wrongInput, setWrongInput] = useState(false);
-    let [scoreGood, setScoreGood] = useState(0);
-    let [scoreBad, setScoreBad] = useState(0);
+    let [word, setWord] = useState({target: ""});
+    let [score, setScore] = useState({ good: 0, bad: 0});
+    let [stop, setStop] = useState(false);
 
     const wordsQueue = useMemo(() => {
         let x = area.replace(/[()«»—–.,~"`';:_-]/g, " ").trim().split(/\s+/);
-        return x == '' ? [] : x;
+        return x == '' ? [] : x.sort(() => Math.random() - 0.5);
     }, [area])
 
-    const startTyper = () => {
-        getWord();
-    }
-
     const getWord = () => {
-        let i = Math.floor(Math.random() * wordsQueue.length);
-        setTargetWord(wordsQueue[i]);
-        wordsQueue.splice(i, 1);
-        console.log(wordsQueue)
+        setInputWord("");
+        if (score.good < wordsQueue.length) {
+            setWord({...word, target: wordsQueue[score.good]})
+            setStop(false);
+        } else {
+            setWord({...word, target: "Вы ввели все слова :)"})
+            setStop(true);
+        }
     }
 
     const wordsCompare = (e) => {
         setInputWord(e.target.value);
-        if (e.target.value === targetWord) {
-            setInputWord("");
-            setScoreGood(scoreGood + 1);
-            getWord();
+        if (e.target.value === word.target) {
+            setScore({...score, good: score.good + 1})
         }
-        if (targetWord.includes(e.target.value, 0)) {
+        if (word.target.includes(e.target.value, 0)) {
             setWrongInput(false);
         } else {
             if (!wrongInput) {
                 setWrongInput(true);
-                setScoreBad(scoreBad + 1);
+                setScore({...score, bad: score.bad + 1})
             }
         }
     }
     
     const resetTyper = () => {
-        console.log('reset');
-        setScoreGood(0);
-        setScoreBad(0);
+        setScore({ good: 0, bad: 0});
+        setWrongInput(false);
+        getWord();
     }
 
 	useEffect(() => {
-		getWord();
+        getWord();
+	}, [score.good]) // eslint-disable-line
+    
+	useEffect(() => {
+        resetTyper();
 	}, [area]) // eslint-disable-line
 
     return (
 		<section className="typer">
 			<div className="wrapper">
-                <div className="typer__word">{wrongInput ? <p className="typer__word-red">{targetWord}</p> : <p>{targetWord}</p>}</div>
+                <div className="typer__word">{wrongInput ? <p className="typer__word-red">{word.target}</p> : <p>{word.target}</p>}</div>
                 <div className="typer__input">
                     <Icons name="icon-go" />
                     <input
                         value={inputWord}
                         onInput={wordsCompare}
                         type="text"
-                        placeholder="Начните вводить" />
+                        placeholder="Начните вводить"
+                        disabled={stop} />
                     <Icons name="icon-reload" onClick={resetTyper} />
                 </div>
                 <div className="typer__score">
-                    <label>Слов: {scoreGood}</label>
-                    <label><span className="typerTimer">00:00</span></label>
-                    <label>Ошибок: {scoreBad}</label>
+                    <label>Слов: {score.good}</label>
+                    <label>Ошибок: {score.bad}</label>
                 </div>
                 <textarea
 						value={area}
